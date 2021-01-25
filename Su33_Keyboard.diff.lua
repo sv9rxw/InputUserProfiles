3b5470b89f20672bd9390dc2e5e9d2f12d9bd9fa
John Velegrakis <sv9rxw@hotmail.com>
2021-01-25 16:24:25 +0200

    Su-33 profile: added

---
+++ Su33_Keyboard.diff.lua
@@ -0,0 +1,497 @@
+local diff = {
+	["keyDiffs"] = {
+		["d100pnilu1627cdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Lock",
+			["removed"] = {
+				[1] = {
+					["key"] = "Enter",
+				},
+			},
+		},
+		["d101pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Weapon Change",
+			["removed"] = {
+				[1] = {
+					["key"] = "D",
+				},
+			},
+		},
+		["d102pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+				},
+			},
+			["name"] = "Next Waypoint, Airfield Or Target",
+		},
+		["d121pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "ASC Direct Control (Cobra)",
+			["removed"] = {
+				[1] = {
+					["key"] = "S",
+				},
+			},
+		},
+		["d1315pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ",",
+				},
+			},
+			["name"] = "Previous Waypoint, Airfield Or Target",
+		},
+		["d147pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Airbrake On",
+			["removed"] = {
+				[1] = {
+					["key"] = "B",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+		},
+		["d148pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Airbrake Off",
+			["removed"] = {
+				[1] = {
+					["key"] = "B",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d165pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Step Up Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageUp",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["d166pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Step Down Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageDown",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["d167pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Step Up Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageUp",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["d168pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Step Down Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageDown",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["d193pnilu194cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Pitch Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "Up",
+				},
+			},
+		},
+		["d195pnilu196cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Pitch Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "Down",
+				},
+			},
+		},
+		["d197pnilu198cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Bank Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "Left",
+				},
+			},
+		},
+		["d199pnilu200cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Bank Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "Right",
+				},
+			},
+		},
+		["d201pnilu202cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Rudder Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "Z",
+				},
+			},
+		},
+		["d203pnilu204cdnilvdnilvpnilvunil"] = {
+			["name"] = "Aircraft Rudder Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "X",
+				},
+			},
+		},
+		["d66pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Step Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageUp",
+				},
+			},
+		},
+		["d67pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Step Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "PageDown",
+				},
+			},
+		},
+		["d73pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Airbrake",
+			["removed"] = {
+				[1] = {
+					["key"] = "B",
+				},
+			},
+		},
+		["d74pnilu75cdnilvdnilvpnilvunil"] = {
+			["name"] = "Wheel Brake On",
+			["removed"] = {
+				[1] = {
+					["key"] = "W",
+				},
+			},
+		},
+		["d84pnilu85cdnilvdnilvpnilvunil"] = {
+			["name"] = "Weapon Fire",
+			["removed"] = {
+				[1] = {
+					["key"] = "Space",
+				},
+			},
+		},
+		["d855pnilu855cdnilvdnilvpnilvunil"] = {
+			["name"] = "Wheel Brake Start",
+			["removed"] = {
+				[1] = {
+					["key"] = "W",
+					["reformers"] = {
+						[1] = "LShift",
+					},
+				},
+			},
+		},
+		["d92pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Designator To Center",
+			["removed"] = {
+				[1] = {
+					["key"] = "I",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["d961pnilu962cdnilvdnilvpnilvunil"] = {
+			["name"] = "Wheel Brake Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+		},
+		["d963pnilu964cdnilvdnilvpnilvunil"] = {
+			["name"] = "Wheel Brake Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+		},
+		["dnilp1032u1034cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num+",
+				},
+			},
+		},
+		["dnilp1033u1034cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num-",
+				},
+			},
+		},
+		["dnilp1035u1037cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Left Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num+",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["dnilp1036u1037cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Left Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num-",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+		},
+		["dnilp1038u1040cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Right Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num+",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["dnilp1039u1040cdnilvdnilvpnilvunil"] = {
+			["name"] = "Throttle Right Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num-",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["dnilp139u230cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num4",
+				},
+			},
+			["name"] = "Scan Zone Left",
+			["removed"] = {
+				[1] = {
+					["key"] = ",",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["dnilp140u230cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num6",
+				},
+			},
+			["name"] = "Scan Zone Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["dnilp141u230cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num8",
+				},
+			},
+			["name"] = "Scan Zone Up",
+			["removed"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["dnilp142u230cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Num2",
+				},
+			},
+			["name"] = "Scan Zone Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num4",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num6",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num8",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "Num2",
+				},
+			},
+		},
+		["dnilp88u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Designator Left",
+			["removed"] = {
+				[1] = {
+					["key"] = ",",
+				},
+			},
+		},
+		["dnilp89u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Designator Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "/",
+				},
+			},
+		},
+		["dnilp90u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Designator Up",
+			["removed"] = {
+				[1] = {
+					["key"] = ";",
+				},
+			},
+		},
+		["dnilp91u235cdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Designator Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ".",
+				},
+			},
+		},
+		["dnilp93u215cdnilvdnilvpnilvunil"] = {
+			["name"] = "Trim: Left Wing Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ",",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["dnilp94u215cdnilvdnilvpnilvunil"] = {
+			["name"] = "Trim: Right Wing Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["dnilp95u215cdnilvdnilvpnilvunil"] = {
+			["name"] = "Trim: Nose Up",
+			["removed"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["dnilp96u215cdnilvdnilvpnilvunil"] = {
+			["name"] = "Trim: Nose Down",
+			["removed"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["dnilp98u215cdnilvdnilvpnilvunil"] = {
+			["name"] = "Trim: Rudder Left",
+			["removed"] = {
+				[1] = {
+					["key"] = "Z",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+		["dnilp99u215cdnilvdnilvpnilvunil"] = {
+			["name"] = "Trim: Rudder Right",
+			["removed"] = {
+				[1] = {
+					["key"] = "X",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
